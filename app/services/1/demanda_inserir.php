e5e23cec51a90c6bde8b7e3249fa8ae175598820
Gabriel Bettencourt <gabriel.vieira@tradesis.com.br>
2023-05-04 15:47:17 -0300

    fix inserir

--- app/services/1/demanda_inserir.php
+++ app/services/1/demanda_inserir.php
@@ -7,24 +7,23 @@ $conexao = conectaMysql();
 $posicao = null;
 $statusDemanda = null;
 
-if (isset($jsonEntrada['idDemanda'])) {
-    $idDemanda = $jsonEntrada['idDemanda'];
+if (isset($jsonEntrada['tituloDemanda'])) {
+    $idCliente = $jsonEntrada['idCliente'];
+    $idUsuario = $jsonEntrada['idUsuario'];
     $tituloDemanda = $jsonEntrada['tituloDemanda'];
     $descricao = $jsonEntrada['descricao'];
-    $prioridade = $jsonEntrada['prioridade'];
     $idTipoStatus = $jsonEntrada['idTipoStatus'];
     $idTipoOcorrencia = $jsonEntrada['idTipoOcorrencia'];
-    $tamanho = $jsonEntrada['tamanho'];
-    $idAtendente = $jsonEntrada['idAtendente'];
 
-    //busca dados tipostatus
+    //busca dados tipostatus    
         $sql2 = "SELECT * FROM tipostatus WHERE idTipoStatus = $idTipoStatus";
         $buscar2 = mysqli_query($conexao, $sql2);
         $row = mysqli_fetch_array($buscar2, MYSQLI_ASSOC);
         $posicao = $row["mudaPosicaoPara"];
         $statusDemanda = $row["mudaStatusPara"];
 
-    $sql = "UPDATE demanda SET prioridade=$prioridade, tituloDemanda='$tituloDemanda', descricao='$descricao', idTipoStatus=$idTipoStatus, idTipoOcorrencia=$idTipoOcorrencia, posicao=$posicao, statusDemanda=$statusDemanda, tamanho='$tamanho', idAtendente=$idAtendente, dataAtualizacaoAtendente=CURRENT_TIMESTAMP() WHERE idDemanda = $idDemanda";    if ($atualizar = mysqli_query($conexao, $sql)) {
+    $sql = "INSERT INTO demanda(prioridade, tituloDemanda, descricao, dataAbertura, idTipoStatus, idTipoOcorrencia, posicao, statusDemanda, idCliente, idUsuario) VALUES (99, '$tituloDemanda','$descricao', CURRENT_TIMESTAMP(), $idTipoStatus, $idTipoOcorrencia, $posicao, $statusDemanda, $idCliente, $idUsuario)";
+    if ($atualizar = mysqli_query($conexao, $sql)) {
         $jsonSaida = array(
             "status" => 200,
             "retorno" => "ok"
