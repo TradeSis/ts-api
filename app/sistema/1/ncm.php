3fdafe9a93b8c6a8e648cb3ab405ecfd91f946d3
Helio Tradesis <helio@tradesis.com.br>
2023-05-31 15:31:39 -0300

    versao 1 arvore

--- app/sistema/1/ncm.php
+++ app/sistema/1/ncm.php
@@ -16,13 +16,55 @@ if (isset($jsonEntrada["Codigo"])) {
 }
 
 
-//echo "-SQL->".$sql."\n";
-
 $rows = 0;
 $buscar = mysqli_query($conexao, $sql);
+
 while ($row = mysqli_fetch_array($buscar, MYSQLI_ASSOC)) {
-  array_push($ncm, $row);
+
+  //echo "-ROW->".json_encode($row)."\n";
+
+  $linhaNcm = array(
+    "Codigo" => $row["Codigo"],
+    "Descricao" => $row["Descricao"],
+    "superior" => $row["superior"],
+    "nivel" => $row["nivel"],
+    "pesquisado" => true
+  );
+
+  array_push($ncm, $linhaNcm);
   $rows = $rows + 1;
+
+  // leitura de subnivel
+  $nivel = $row["nivel"];
+  $proxnivel = $nivel + 1;
+  $superior = $row["Codigo"];
+  if ($nivel < 4) {
+    $sql1 = "SELECT * FROM ncm where ncm.superior = " . $superior . " and ncm.nivel = " . $proxnivel;
+
+    $buscar1 = mysqli_query($conexao, $sql1);
+    while ($row1 = mysqli_fetch_array($buscar1, MYSQLI_ASSOC)) {
+        $linhaNcm1 = array(
+          "Codigo" => $row1["Codigo"],
+          "Descricao" => $row1["Descricao"],
+          "superior" => $row1["superior"],
+          "nivel" => $row1["nivel"],
+          "pesquisado" => true
+        );
+        array_push($ncm, $linhaNcm1);
+
+        // nivel 3
+
+
+
+
+            // nivel 4
+
+            
+      }
+  
+  }
+
+
 }
 
 $jsonSaida = $ncm;
