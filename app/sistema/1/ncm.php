1d2af0ba53ef8593f69ddeef8aeceb87ea4372a5
Gabriel Bettencourt <gabriel.vieira@tradesis.com.br>
2023-06-09 13:59:48 -0300

    update table fiscais

--- app/sistema/1/ncm.php
+++ app/sistema/1/ncm.php
@@ -10,41 +10,43 @@ function Desce($conexao, $superior, &$ncm, &$ncmIds)
 
     while ($row = mysqli_fetch_array($buscar, MYSQLI_ASSOC)) {
         $linhaNcm = [
-            "Codigo" => $row["Codigo"],
+            "codigoNcm" => $row["codigoNcm"],
             "Descricao" => $row["Descricao"],
             "superior" => $row["superior"],
             "nivel" => $row["nivel"],
             "ultimonivel" => $row["ultimonivel"],
+            "ncm" => $row["ncm"],
             "pesquisado" => false
         ];
 
-        if (!in_array($row["Codigo"], $ncmIds)) {
+        if (!in_array($row["codigoNcm"], $ncmIds)) {
             array_push($ncm, $linhaNcm);
-            array_push($ncmIds, $row["Codigo"]);
+            array_push($ncmIds, $row["codigoNcm"]);
         }
 
-        Desce($conexao, $row["Codigo"], $ncm, $ncmIds);
+        Desce($conexao, $row["codigoNcm"], $ncm, $ncmIds);
     }
 }
 
 function Sobe($conexao, $superior, &$ncm, &$ncmIds)
 {
-    $sql = "SELECT * FROM fisncm WHERE fisncm.Codigo = $superior";
+    $sql = "SELECT * FROM fisncm WHERE fisncm.codigoNcm = $superior";
     $buscar = mysqli_query($conexao, $sql);
 
     while ($row = mysqli_fetch_array($buscar, MYSQLI_ASSOC)) {
         $linhaNcm = [
-            "Codigo" => $row["Codigo"],
+            "codigoNcm" => $row["codigoNcm"],
             "Descricao" => $row["Descricao"],
             "superior" => $row["superior"],
             "nivel" => $row["nivel"],
             "ultimonivel" => $row["ultimonivel"],
+            "ncm" => $row["ncm"],
             "pesquisado" => false
         ];
 
-        if (!in_array($row["Codigo"], $ncmIds)) {
+        if (!in_array($row["codigoNcm"], $ncmIds)) {
             array_push($ncm, $linhaNcm);
-            array_push($ncmIds, $row["Codigo"]);
+            array_push($ncmIds, $row["codigoNcm"]);
         }
 
         if ($row["nivel"] != 1 || $row["ultimonivel"] != 0) {
@@ -56,8 +58,8 @@ function Sobe($conexao, $superior, &$ncm, &$ncmIds)
 //**********SQL QUERY **********/
 $sql = "SELECT * FROM fisncm";
 $where = " WHERE ";
-if (isset($jsonEntrada["Codigo"])) {
-    $sql .= $where . " fisncm.Codigo = " . $jsonEntrada["Codigo"];
+if (isset($jsonEntrada["codigoNcm"])) {
+    $sql .= $where . " fisncm.codigoNcm = " . $jsonEntrada["codigoNcm"];
     $where = " AND ";
 }
 if (isset($jsonEntrada["Descricao"])) {
@@ -70,11 +72,12 @@ $buscar = mysqli_query($conexao, $sql);
 
 while ($row = mysqli_fetch_array($buscar, MYSQLI_ASSOC)) {
     $linhaNcm = [
-        "Codigo" => $row["Codigo"],
+        "codigoNcm" => $row["codigoNcm"],
         "Descricao" => $row["Descricao"],
         "superior" => $row["superior"],
         "nivel" => $row["nivel"],
         "ultimonivel" => $row["ultimonivel"],
+        "ncm" => $row["ncm"],
         "pesquisado" => true
     ];
 
@@ -83,16 +86,15 @@ while ($row = mysqli_fetch_array($buscar, MYSQLI_ASSOC)) {
     }
 
     if ($row["nivel"] < 6) {
-        Desce($conexao, $row["Codigo"], $ncm, $ncmIds);
+        Desce($conexao, $row["codigoNcm"], $ncm, $ncmIds);
     }
 
 
-    if (!in_array($row["Codigo"], $ncmIds)) {
+    if (!in_array($row["codigoNcm"], $ncmIds)) {
         array_push($ncm, $linhaNcm);
-        array_push($ncmIds, $row["Codigo"]);
+        array_push($ncmIds, $row["codigoNcm"]);
     }
     $rows++;
 }
 
 $jsonSaida = $ncm;
-usort($jsonSaida, fn($a, $b) => $a['nivel'] - $b['nivel']);
