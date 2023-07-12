d46396b1d307441fe25a6cc9a2bcdb261183250f
Helio Tradesis <helio@tradesis.com.br>
2023-07-12 16:20:21 -0300

    teste null

--- app/services/1/tarefas_alterar.php
+++ app/services/1/tarefas_alterar.php
@@ -9,12 +9,16 @@ if (isset($jsonEntrada['idTarefa'])) {
     $idTarefa = $jsonEntrada['idTarefa'];
     $idDemanda = $jsonEntrada['idDemanda'];
     $tituloTarefa = $jsonEntrada['tituloTarefa'];
-    $dataCobrado = $jsonEntrada['dataCobrado'];
-    $horaInicioCobrado = $jsonEntrada['horaInicioCobrado'];
-    $horaFinalCobrado = $jsonEntrada['horaFinalCobrado'];
+   // $dataCobrado = $jsonEntrada['dataCobrado'];
+   // $horaInicioCobrado = $jsonEntrada['horaInicioCobrado'];
+   // $horaFinalCobrado = $jsonEntrada['horaFinalCobrado'];
+    $dataCobrado = (is_null($jsonEntrada['dataCobrado']) ? null : $jsonEntrada['dataCobrado']);
+    $horaInicioCobrado = (is_null($jsonEntrada['horaInicioCobrado']) ? null : $jsonEntrada['horaInicioCobrado']);
+    $horaFinalCobrado = (is_null($jsonEntrada['horaFinalCobrado']) ? null : $jsonEntrada['horaFinalCobrado']);
+  
     $idTipoOcorrencia = $jsonEntrada['idTipoOcorrencia'];
 
-    $sql = "UPDATE `tarefa` SET `tituloTarefa`='$tituloTarefa',`dataCobrado`=$dataCobrado, `horaInicioCobrado`='$horaInicioCobrado', `horaFinalCobrado`='$horaFinalCobrado', `idTipoOcorrencia`=$idTipoOcorrencia WHERE `idTarefa` = $idTarefa";
+    $sql = "UPDATE `tarefa` SET `tituloTarefa`='$tituloTarefa',`dataCobrado`=$dataCobrado, `horaInicioCobrado`=$horaInicioCobrado, `horaFinalCobrado`=$horaFinalCobrado, `idTipoOcorrencia`=$idTipoOcorrencia WHERE `idTarefa` = $idTarefa";
 
     if (mysqli_query($conexao, $sql)) {
         $sql2 = "UPDATE `demanda` SET `idTipoOcorrencia`=$idTipoOcorrencia WHERE `idDemanda` = $idDemanda";
